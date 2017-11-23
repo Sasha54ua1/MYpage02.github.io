ca145c80158bf67aa1868ca961f1a70d9f1fb1f9
Sasha54ua1 <Sasha54ua@gmail.com>
2017-11-23 14:05:28 +0200

    commit

---
+++ 1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Untitled Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1 text/html; charset=windows-1251">
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+	
+	<link href="css/bootstrap.css" rel="stylesheet">
+
+</head>
+ 
+<body>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<div class="container">
+		<pre><? include ("message.txt")?></pre>
+	</div>
+</body>
+</html>
---
+++ qqqqqq.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+</head>
+ 
+<body>
+
+<form action="vopros.php" method="post">
+<pre><? include ("message.txt")?></pre>
+</form>
+
+</body>
+</html>
